c8afbd2f9f5442244691e7a24fb718b83039b686
Saddam Akhtar <saddamakhtar88@gmail.com>
2020-03-20 12:30:17 +0100

    Enabled caching in fetch catalogs API

--- app/data-service/catalog-service.js
+++ app/data-service/catalog-service.js
@@ -1,18 +1,52 @@
 import {EnvironmentConfiguration} from '../EnvironmentConfiguration';
+import AsyncStorage from '@react-native-community/async-storage';
+import {Cache} from 'react-native-cache';
 
 export class CatalogDataService {
   constructor() {
     baseurl = EnvironmentConfiguration.CatalogAPI_Base_URL;
     catalogEndPoint = baseurl + 'Catalog';
+
+    cache = new Cache({
+      namespace: 'ProductCatalog',
+      policy: {
+        maxEntries: 500,
+      },
+      backend: AsyncStorage,
+    });
   }
 
   getCatalogs() {
-    return fetch(catalogEndPoint).then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw 'Error in getCatalogs. Status: ' + response.status;
-      }
+    return new Promise((resolve, reject) => {
+      fetch(catalogEndPoint)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            cache.getItem(catalogEndPoint, function(err, value) {
+              if (value) {
+                resolve(JSON.parse(value));
+              } else {
+                reject('Error in getCatalogs. Status: ' + response.status);
+              }
+            });
+          }
+        })
+        .then(result => {
+          cache.setItem(catalogEndPoint, JSON.stringify(result), function(
+            _,
+          ) {});
+          resolve(result);
+        })
+        .catch(error => {
+          cache.getItem(catalogEndPoint, function(err, value) {
+            if (value) {
+              resolve(JSON.parse(value));
+            } else {
+              reject('Error in getCatalogs.');
+            }
+          });
+        });
     });
   }
 
